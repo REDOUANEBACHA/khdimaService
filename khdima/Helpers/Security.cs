ffdba79d05a2a6d950394b2f00149bd4f1e1284d
AIT EL BACHA <r.aitelbacha@gmail.com>
2024-02-25 20:36:13 +0100

    feat: add security classe for  GenerateJwtToken &&  DecodedToken

---
+++ khdima/Helpers/Security.cs
@@ -0,0 +1,64 @@
+﻿
+using khdima.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace khdima.Helpers
+{
+    public class Security
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public Security(IConfiguration configuration)
+        {
+  
+            _configuration = configuration;
+        }
+
+        public string GenerateJwtToken(Users user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                       claims: new[] {
+                    new Claim("idUser", user.id.ToString()),
+                    new Claim("name", $"{user.first_name} {user.last_name}"),
+                     new Claim("role", $"Admin"),
+                },
+                expires: DateTime.UtcNow.AddMinutes(10),
+                signingCredentials: signIn);
+
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+
+        public IEnumerable<Claim> DecodedToken(string jwtToken)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+
+            tokenHandler.ValidateToken(jwtToken, new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ClockSkew = TimeSpan.Zero
+            }, out SecurityToken validatedToken);
+
+            var jwtTokenObject = (JwtSecurityToken)validatedToken;
+
+            var claims = jwtTokenObject.Claims;
+            return claims;
+
+        }
+
+
+    }
+}
