b11c15382914241dcdd211928d10cd422bde22b7
AIT EL BACHA <r.aitelbacha@gmail.com>
2024-02-25 20:22:23 +0100

    feat: add jsonIgnore for  desabled the attribut in response json

--- khdima/Models/Users.cs
+++ khdima/Models/Users.cs
@@ -1,4 +1,6 @@
-﻿namespace khdima.Models
+﻿using System.Text.Json.Serialization;
+
+namespace khdima.Models
 {
     public class Users
     {
@@ -8,13 +10,14 @@
         public string phone { get; set; }
         public DateTime birthday_date { get; set; }
         public string email { get; set; }
-        public string password { get; set; }
-        public string? tmp_password { get; set; }
-        public DateTime? password_send_date { get; set; }
+        [JsonIgnore] public string password { get; set; }
+        [JsonIgnore] public string? tmp_password { get; set; }
+        [JsonIgnore] public DateTime? password_send_date { get; set; }
         public DateTime? last_access { get; set; }
         public DateTime? first_access { get; set; }
         public DateTime created_at { get; set; } 
-        public DateTime updated_at { get; set; } 
+        public DateTime updated_at { get; set; }
+         
     }
 }
 
