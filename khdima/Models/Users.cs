f30576ece96d6d5579ceb236e1482f92b1f076b9
AIT EL BACHA <r.aitelbacha@gmail.com>
2024-03-04 23:04:14 +0100

    ci: add attributes

--- khdima/Models/Users.cs
+++ khdima/Models/Users.cs
@@ -2,22 +2,26 @@
 
 namespace khdima.Models
 {
+  
+
     public class Users
     {
         public int id { get; set; }
+        public byte[] img { get; set; }
         public string first_name { get; set; }
         public string last_name { get; set; }
         public string phone { get; set; }
         public DateTime birthday_date { get; set; }
+        public string sexe { get; set; } 
         public string email { get; set; }
         [JsonIgnore] public string password { get; set; }
         [JsonIgnore] public string? tmp_password { get; set; }
         [JsonIgnore] public DateTime? password_send_date { get; set; }
         public DateTime? last_access { get; set; }
         public DateTime? first_access { get; set; }
-        public DateTime created_at { get; set; } 
+        public DateTime created_at { get; set; }
         public DateTime updated_at { get; set; }
-         
+        public string newsletter_status { get; set; }
     }
 }
 
