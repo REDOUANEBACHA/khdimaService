7a3f046977023d4223ddf3ffe206965f3727238a
AIT EL BACHA <r.aitelbacha@gmail.com>
2024-03-04 23:11:58 +0100

    feat: UserRole class

---
+++ khdima/Models/type/UserRole.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+
+namespace khdima.Models.type
+{
+    public class UserRole
+    {
+        public User_has_role UserHasRole { get; set; }
+        public Users User { get; set; }
+        public Roles Role { get; set; }
+
+    
+    }
+}
